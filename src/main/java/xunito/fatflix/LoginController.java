d7a91f280c090f580fc046e20fbfb0dce48441cb
Eduardo-H <edu-henrique2014@hotmail.com>
2021-01-18 19:57:04 -0300

    Login window modification

--- src/main/java/xunito/fatflix/LoginController.java
+++ src/main/java/xunito/fatflix/LoginController.java
@@ -1,5 +1,21 @@
 package xunito.fatflix;
 
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class LoginController {
+	@FXML
+	private Button loginBtn;
 	
+	
+	@FXML
+	private void login() {
+		try {
+			App.setRoot("main");
+		} catch (IOException e) {
+			System.err.println("Not able to open the main window");
+		}
+	}
 }
--- src/main/java/xunito/fatflix/MainController.java
+++ src/main/java/xunito/fatflix/MainController.java
@@ -6,7 +6,11 @@ import javafx.fxml.FXML;
 public class MainController {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("login");
+    private void logout() {
+    	try {
+    		App.setRoot("login");
+    	} catch (IOException e) {
+    		System.err.println("Not able to open the login window");
+    	}
     }
 }
