3ded99a8e918edc0f19f7a5ebae9000044fbcaf3
Eduardo-H <edu-henrique2014@hotmail.com>
2021-02-12 15:27:47 -0300

    Sing up page created

--- src/main/java/xunito/fatflix/RegisterController.java
+++ src/main/java/xunito/fatflix/RegisterController.java
@@ -1,19 +1,17 @@
 package xunito.fatflix;
 
 import java.io.IOException;
-
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 
-public class LoginController {
+public class RegisterController {
+
 	@FXML
 	private Button loginBtn;
 	private Button singUpBtn;
 	
 	@FXML
-	private void login() {
+	private void singUp() {
 		try {
 			App.setRoot("main");
 		} catch (IOException e) {
@@ -22,11 +20,11 @@ public class LoginController {
 	}
 	
 	@FXML
-	private void singUp() {
+	private void login() {
 		try {
-			App.setRoot("register");
+			App.setRoot("login");
 		} catch (IOException e) {
-			System.err.println("Not able to open the sing up window");
+			System.err.println("Not able to open the login window");
 		}
 	}
 }
