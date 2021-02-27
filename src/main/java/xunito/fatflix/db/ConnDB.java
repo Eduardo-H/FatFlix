25c68f7933bde8b4b815000b53d1f543464426b4
Eduardo-H <edu-henrique2014@hotmail.com>
2021-02-27 13:45:42 -0300

    ConnDB class implemented

---
+++ src/main/java/xunito/fatflix/db/ConnDB.java
@@ -0,0 +1,89 @@
+package xunito.fatflix.db;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import javafx.scene.control.Alert;
+import xunito.fatflix.AlertUtil;
+
+public class ConnDB {
+	private static EntityManagerFactory entityManagerFactory;
+	private static EntityManager entityManager;
+
+	private static EntityManagerFactory getEntityManagerFactory() {
+		if (entityManagerFactory == null)
+			entityManagerFactory = Persistence.createEntityManagerFactory("fatflix_db");
+		return entityManagerFactory;
+	}
+
+	public static EntityManager getEntityManager() {
+		if (entityManager == null)
+			entityManager = getEntityManagerFactory().createEntityManager();
+		return entityManager;
+	}
+
+	public static void closeConn() {
+		if (entityManager != null)
+			entityManager.close();
+		if (entityManagerFactory != null)
+			entityManagerFactory.close();
+	}
+
+	public static void initDB() {
+		for (User u : consumeAPI(consultAPI()))
+			new UserDAO().persist(u);
+	}
+
+	public static List<User> consumeAPI(List<String> users) {
+		List<User> result = new ArrayList<User>();
+		for (int lineIndex = 0; lineIndex < users.size(); lineIndex++) {
+			String line = users.get(lineIndex);
+			if (line.contains("username")) {
+				String username = processJSONLine(line);
+				lineIndex++;
+				line = users.get(lineIndex);
+				String password = processJSONLine(line);
+				User user = new User(username, password, "");
+				result.add(user);
+			}
+		}
+		return result;
+	}
+
+	private static String processJSONLine(String line) {
+		String[] dividedLine = line.split(":");
+		String username = dividedLine[1];
+		username = username.replace(",", " ");
+		username = username.replace("\"", " ");
+		username = username.trim();
+		return username;
+	}
+
+	private static List<String> consultAPI() {
+		List<String> result = new ArrayList<>();
+		try {
+			URL url = new URL("http://www.lucasbueno.com.br/steam.json");
+			URLConnection uc = url.openConnection();
+			InputStreamReader input = new InputStreamReader(uc.getInputStream());
+			BufferedReader in = new BufferedReader(input);
+			String inputLine;
+
+			while ((inputLine = in.readLine()) != null)
+				result.add(inputLine);
+
+			in.close();
+		} catch (Exception e) {
+			Alert alert = AlertUtil.error("Error", "Not able to connect to the API", "Error trying accessing the API", e);
+			alert.showAndWait();
+		}
+		return result;
+	}
+}
