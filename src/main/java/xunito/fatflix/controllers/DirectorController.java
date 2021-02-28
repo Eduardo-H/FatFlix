bbb8113d5868e5c44cddcb5e6323708d03eb8426
Eduardo-H <edu-henrique2014@hotmail.com>
2021-02-28 19:42:18 -0300

    Director window created

---
+++ src/main/java/xunito/fatflix/controllers/DirectorController.java
@@ -0,0 +1,33 @@
+package xunito.fatflix.controllers;
+
+import javafx.fxml.FXML;
+import xunito.fatflix.App;
+
+public class DirectorController {
+	
+	@FXML
+	public void openDirectorRegistration() {
+		App.setRoot("directorRegister");
+	}
+	
+	@FXML
+    private void logout() {
+    	App.changeResizable();
+    	App.setRoot("login");
+    }
+	
+	@FXML
+	public void openHomeWindow() {
+		App.setRoot("main");
+	}
+	
+	@FXML
+	public void openMoviesWindow() {
+		App.setRoot("movies");
+	}
+	
+	@FXML
+	public void openTVShowsWindow() {
+		App.setRoot("tvShows");
+	}
+}
